775268ebaa03dcc902070568a45850bb3b197d11
Nick536363 <3383371nik@gmail.com>
2025-09-02 19:03:40 +0200

    Добавил модель PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,3 +6,8 @@ class Pokemon(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to="pokemons")
     def __str__(self):
         return f"{self.title}"
+
+
+class PokemonEntity(models.Model):
+    lat = models.FloatField(default=0.0)
+    lon = models.FloatField(default=0.0)
