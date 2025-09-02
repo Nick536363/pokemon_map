616e58757df0e3fe550ada84caecf8167b1b3bcd
Nick536363 <3383371nik@gmail.com>
2025-09-02 18:56:58 +0200

    Добавил поле для установки картинки Покемона

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,6 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.CharField(default="Покемон", max_length=50)
-
+    image = models.ImageField(null=True, blank=True, upload_to="pokemons")
     def __str__(self):
         return f"{self.title}"
