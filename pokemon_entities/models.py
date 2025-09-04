35cd85a7984b54eafcceab442a8e0c1998f56b54
Nick536363 <3383371nik@gmail.com>
2025-09-04 18:07:38 +0200

    добавил поля для времени появления/исчезновения покемонов

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,4 +1,5 @@
 from django.db import models  # noqa F401
+from datetime import datetime
 
 # your models here
 class Pokemon(models.Model):
@@ -9,6 +10,8 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, default=None)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True, blank=False)
     lat = models.FloatField(default=0.0)
     lon = models.FloatField(default=0.0)
+    appeared_at = models.DateTimeField(blank=False, default=datetime.now())
+    disappeared_at = models.DateTimeField(blank=False, default=datetime.now())
