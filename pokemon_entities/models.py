e8e540803437e5549c0c57824fac5a017f0af641
Nick536363 <3383371nik@gmail.com>
2025-09-04 18:21:39 +0200

    Добавил статы для модели покемона

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(default="Покемон", max_length=50)
+    title = models.CharField(max_length=50, default="Покемон")
     image = models.ImageField(null=True, blank=True, upload_to="pokemons")
     def __str__(self):
         return f"{self.title}"
@@ -11,7 +11,12 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True, blank=False)
-    lat = models.FloatField(default=0.0)
-    lon = models.FloatField(default=0.0)
+    lat = models.FloatField(blank=False, default=0.0)
+    lon = models.FloatField(blank=False, default=0.0)
     appeared_at = models.DateTimeField(blank=False, default=datetime.now())
     disappeared_at = models.DateTimeField(blank=False, default=datetime.now())
+    level = models.IntegerField(blank=False, default=0)
+    health = models.IntegerField(blank=False, default=0)
+    damage = models.IntegerField(blank=False, default=0)
+    defense = models.IntegerField(blank=False, default=0)
+    stamina = models.IntegerField(blank=False, default=0)
