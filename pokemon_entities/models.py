b9b092d5622038758720639ec75dabdbb03065e7
Nick536363 <3383371nik@gmail.com>
2025-09-30 16:51:09 +0200

    Добавил related_name для покемона

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -14,7 +14,7 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, verbose_name="Покемон", on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, verbose_name="Покемон", on_delete=models.CASCADE, related_name="pokemons")
     lat = models.FloatField("Широта")
     lon = models.FloatField("Долгота")
     appeared_at = models.DateTimeField("Время появление", default=datetime.now())
